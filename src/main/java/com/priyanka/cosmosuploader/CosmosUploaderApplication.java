a69ea754e3cd87f27471d54737ed02f03295958b
priyanka010392 <gudia.agar01@gmail.com>
2021-07-08 23:13:16 +0530

    base structure

---
+++ src/main/java/com/priyanka/cosmosuploader/CosmosUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.priyanka.cosmosuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanka/cosmosuploader/CosmosUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanka.cosmosuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
